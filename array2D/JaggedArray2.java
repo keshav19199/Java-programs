66edb6879774d3800dcf0599927d4a2a8a0980d9
keshav19199 <keshavwadhavane19@gmail.com>
2025-09-25 15:07:57 +0530

    Jagged Array

---
+++ array2D/JaggedArray2.java
@@ -0,0 +1,35 @@
+//Create any Jadded array.
+
+package java_prog.array2D;
+
+public class JaggedArray2 {
+	public static void main(String[] args) {
+		
+		int[][] arr=new int[4][];
+		int counter=1;
+		
+		arr[0]=new int[4];
+		arr[1]=new int[3];
+		arr[2]=new int[2];	
+		arr[3]=new int[1];
+			
+		for(int i=0; i<arr.length; i++)
+		{
+			for(int j=0; j<arr[i].length; j++)
+			{
+				arr[i][j]=counter;
+				counter++;
+			}
+		}
+		for(int i=0; i<arr.length; i++)
+		{
+			for(int j=0; j<arr[i].length; j++)
+			{
+				System.out.print(arr[i][j]+" ");
+			}
+			System.out.println();
+		}
+		
+	}
+
+}
