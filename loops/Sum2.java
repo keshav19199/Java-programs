b12f5389dee202425488946d882ce070577ceb9b
keshav19199 <keshavwadhavane19@gmail.com>
2025-09-29 15:13:40 +0530

    Sum

---
+++ loops/Sum2.java
@@ -0,0 +1,18 @@
+//Total number of sum
+
+package java_prog.loops;
+
+public class Sum2 {
+
+	public static void main(String[] args) {
+		
+		int sum=0;
+		
+		for(int i=0; i<=500; i++)
+		{
+			sum+=i;
+		}
+		System.out.println(sum);
+	}
+
+}
