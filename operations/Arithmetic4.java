978a2cb03774616fa78d4e6c8e8a7a9663dd80b3
keshav19199 <keshavwadhavane19@gmail.com>
2025-09-30 14:40:41 +0530

    Add,sub,div,mult,mod

---
+++ operations/Arithmetic4.java
@@ -0,0 +1,19 @@
+package java_prog.operations;
+
+public class Arithmetic4 {
+
+	public static void main(String[] args) {
+
+		int num1=12;
+		int num2=4;
+		
+		System.out.println(num1+num2);		
+		System.out.println(num1-num2);
+		System.out.println(num1*num2);
+		System.out.println(num1/num2);
+		System.out.println(num1%num2);
+		System.out.println("Done..");
+
+	}
+
+}
---
+++ operations/Bitwise.java
@@ -0,0 +1,23 @@
+package java_prog.operations;
+
+public class Bitwise {
+	public static void main(String[] args) {
+		
+		int a=60;
+		int b=13;
+		
+		System.out.println(a&b);
+		System.out.println(a|b);
+		System.out.println(a^b);
+		System.out.println(~a);
+		System.out.println(~b);
+		System.out.println(a<<2);
+		System.out.println(a>>2);
+		System.out.println(a>>>2);
+
+
+
+
+	}
+
+}
