71e8137ba9259f5eed6d21f54f4c4019c7f6167d
keshav19199 <keshavwadhavane19@gmail.com>
2025-09-30 17:56:40 +0530

    While

--- loops/WhileLoop.java
+++ loops/WhileLoop.java
@@ -1,5 +1,18 @@
 package java_prog.loops;
 
 public class WhileLoop {
+	public static void main(String[] args) {
+		
+		int num=1;
+		int count=0;
+		
+		while(num<10)
+		{
+			System.out.println(" "+num);
+			num++;
+			count++;
+		}
+		System.out.println("Total: "+count);
+	}
 
 }
