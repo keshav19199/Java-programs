1424acb871ee076146de8607e45a7aca6ff23c13
keshav19199 <keshavwadhavane19@gmail.com>
2025-10-06 22:32:13 +0530

    Input Parameter

---
+++ Classes/Input_Parameter.java
@@ -0,0 +1,23 @@
+package java_prog.Classes;
+
+public class Input_Parameter {
+
+	public static void main(String[] args) {
+
+		Railway rail=new Railway();
+		rail.add(30,20);
+		
+		Railway rail2=new Railway();
+		rail.add(100,900);
+		
+	}
+
+}
+class Railway
+{
+	public void add(int i, int j)
+	{
+		System.out.println(i+j);	
+	}
+
+}
