2c801f201e5a58f2de8816b4aea5caece42e9c72
keshav19199 <keshavwadhavane19@gmail.com>
2025-10-10 11:49:36 +0530

    string work

---
+++ string/StringWorking.java
@@ -0,0 +1,14 @@
+package java_prog.string;
+
+public class StringWorking {
+
+	public static void main(String[] args) {
+
+		
+		String str="ab";
+		str="cd";
+		
+		System.out.println(str);
+	}
+
+}
