ba8206413bb35a081a7aca8e7b8d26e2891e3971
keshav19199 <keshavwadhavane19@gmail.com>
2025-11-04 17:36:57 +0530

    construtor

--- constructors/First.java
+++ constructors/First.java
@@ -5,15 +5,29 @@ public class First {
 	public static void main(String[] args) {
 
 		Book book=new Book(); 	//Constructor call
-			
+		book.Book('3','4');
+		book.Book(3.4);
+		book.Book("d");
 		
 	}
 
 }
 class Book
 {
-	public  Book()
+	public Book()
 	{
 		System.out.println("Good morning...");
+	}
+	public void Book(double d)
+	{
+		System.out.println("it is double value");
+	}
+	public void Book(char a, char b)
+	{
+		System.out.println("it is character");
+	}
+	public void Book(String s)
+	{
+		System.out.println("It is a string");
 	}
 }
