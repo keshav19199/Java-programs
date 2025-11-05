4d745a1742b60065bc7fcf4740175088e0f53f8b
keshav19199 <keshavwadhavane19@gmail.com>
2025-11-05 22:08:38 +0530

    polymorphism

---
+++ polymorphism/Overloading.java
@@ -0,0 +1,24 @@
+package java_prog.polymorphism;
+
+public class Overloading {
+
+	public static void main(String[] args) {
+
+		Car c=new Car();
+		c.add(4, 5);
+		c.sub("d","d", 0);
+	}
+
+}
+class Car
+{
+	public void add(int a, int b)		//overloading is a determine by signature. 
+	{										//it check to no.of param,sequence and type of parameter.
+		System.out.println(a+b);
+	}
+	
+	public void sub(String z, String y, int w)
+	{
+		System.out.println("string - string - int");
+	}
+}
