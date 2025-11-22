0036a6d672bd6299d8a6e6b2f7f2f75f59229663
keshav19199 <keshavwadhavane19@gmail.com>
2025-11-22 11:01:35 +0530

    multi Exception

---
+++ exceptions/Exception4.java
@@ -0,0 +1,53 @@
+package java_prog.exceptions;
+
+public class Exception4 {
+
+	public static void main(String[] args) {
+
+		int age=41;
+		
+		try {
+			if(age>40 && age <60)
+			{
+				throw new NumberException();
+			}
+			else if(age>60 && age<80)
+			{
+				throw new Number2Exception();
+			}
+			else
+			{
+				System.out.println("You are young..");
+			}
+			
+			
+		}
+			catch(NumberException ne)
+			{
+				System.out.println(ne.getMessage());
+			}
+			catch(Number2Exception ne)
+			{
+				System.out.println(ne.getMessage());
+			}
+		}
+	}
+
+
+class NumberException extends Exception
+{
+	public NumberException()
+	{
+		super("This is number 1 exception..");
+	}
+}
+
+class Number2Exception extends Exception
+{
+	public Number2Exception()
+	{
+		super("2 Number Exception");
+	}
+}
+
+
