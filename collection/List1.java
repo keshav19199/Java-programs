ab808d653b2d90a24d8ccb3e16c02a0f066555a5
keshav19199 <keshavwadhavane19@gmail.com>
2025-12-01 23:52:55 +0530

    List

---
+++ collection/List1.java
@@ -0,0 +1,15 @@
+package java_prog.collection;
+
+import java.util.List;
+import java.util.ArrayList;
+
+public class List1 {
+
+	public static void main(String[] args) {
+
+		List<String> list=new ArrayList<String>();
+		
+		System.out.println(list.add("qwerty"));
+	}
+
+}
