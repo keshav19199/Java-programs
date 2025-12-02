6d83df32203d1e36dceaf7a014ce33ee5dcb89f6
keshav19199 <keshavwadhavane19@gmail.com>
2025-12-02 10:06:20 +0530

    list

--- collection/List2.java
+++ collection/List2.java
@@ -8,13 +8,16 @@ public class List2 {
 	public static void main(String[] args) {
 
 		List<Integer> list= new ArrayList<Integer>();
-		
-		
+		List<Character> list1 =new ArrayList<Character>();
+		List<Boolean> list2 =new ArrayList<Boolean>();
+
+				
 		System.out.println(list.add(21421));
+		System.out.println(list1.add('s'));
+		System.out.println(list2.add(null));
+		
 		System.out.println(list);
-		
-		
-		
+				
 		
 	}
 
