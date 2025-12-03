6d2ecc8ff1896143f98168ec272340ae851ff825
keshav19199 <keshavwadhavane19@gmail.com>
2025-12-03 14:59:35 +0530

    isEmpty method

---
+++ collection/ArrayList3.java
@@ -0,0 +1,16 @@
+package java_prog.collection;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ArrayList3 {
+	public static void main(String args[]) {
+		
+		ArrayList arr=new ArrayList<>();
+	
+		System.out.println(arr.isEmpty());
+		
+		System.out.println(arr);
+	}
+
+}
