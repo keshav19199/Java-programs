8431b9c1e0dd98dc89636fbbd3678b1d80361e53
keshav19199 <keshavwadhavane19@gmail.com>
2025-12-09 21:57:25 +0530

    Even odd

---
+++ Logoical/EvenOdd.java
@@ -0,0 +1,22 @@
+package java_prog.Logoical;
+
+public class EvenOdd {
+
+	public static void main(String[] args) {
+
+		
+		for(int i=0; i<=100; i++)
+		{
+			if(i%2==0)
+			{
+				System.out.println("Even "+i);
+			}
+			else
+			{
+				System.out.println("Odd: "+i);
+			}
+		}
+		
+	}
+
+}
