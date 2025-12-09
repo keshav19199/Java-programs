3520417747b0739eacd3738634904915856faaf1
keshav19199 <keshavwadhavane19@gmail.com>
2025-12-09 20:13:17 +0530

    Bank Details

--- projects/BankDetails.java
+++ projects/BankDetails.java
@@ -4,9 +4,10 @@ import java.util.Scanner;
 
 public class BankDetails {
 
+	static Scanner sc=new Scanner(System.in);
 	public static void main(String[] args) {
 
-		Scanner sc=new Scanner(System.in);
+		
 		double Balance=0;
 		int choice;
 		boolean isRunning=true;
@@ -29,18 +30,61 @@ public class BankDetails {
 		switch(choice)
 		{
 		case 1 -> showBalance(Balance);
-		case 2 -> System.out.println("Deposite");
-		case 3 -> System.out.println("Withdraw");
+		case 2 -> Balance = Balance + Deposite();
+		case 3 -> Balance = Balance - Withdraw(Balance);
 		case 4 -> isRunning = false;
 		
 		default -> System.out.println("Invalid Choice..");
 		}
 		}
+		System.out.println("*****************************");
+		System.out.println("Thank you! Have a nice day!...");
+		System.out.println("*****************************");
 		sc.close();
 	}
 
 	static void showBalance(double Balance)
 	{
 		System.out.println("$"+Balance);
+		
 	}
+	static double Deposite()
+	{
+		double amount;
+		System.out.println("Enter an amount to be deposited: ");
+		amount = sc.nextDouble();	
+		
+		if(amount<0)
+		{
+			System.out.println("Amount can't be negative");
+			return 0;
+		}
+		else {
+			return amount;
+		}
+	}
+	
+	
+	static double Withdraw(double Balance)
+	{
+		double amount;
+		System.out.println("Enter amount to withdraw: ");
+		amount = sc.nextDouble();
+		
+		if(amount > Balance)
+		{
+			System.out.println("Insufficient amount");
+			return 0;
+		}
+		else if(amount < 0)
+		{
+			System.out.println("Amount can't be negative");
+			return 0;
+		}
+		else {		
+			return amount;
+		}
+		
+	}
+	
 }
