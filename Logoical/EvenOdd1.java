3b95f8f7d8a2eae8023fa3c50b5940ed4321135a
keshav19199 <keshavwadhavane19@gmail.com>
2025-12-10 21:08:22 +0530

    Even odd

---
+++ Logoical/EvenOdd1.java
@@ -0,0 +1,18 @@
+// find the number be odd or even
+package java_prog.Logoical;
+
+public class EvenOdd1 {
+
+	public static void main(String[] args) {
+
+	long num=1999081332;
+	if(num%2==0)
+	{
+		System.out.println("Sum : "+num);
+	}
+	else {
+		System.out.println("Odd : "+num);
+	}
+	}
+
+}
