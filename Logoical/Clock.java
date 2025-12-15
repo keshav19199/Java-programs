090d4c5c76ee87ccfb4ebe5f2228dc63d62a956b
keshav19199 <keshavwadhavane19@gmail.com>
2025-12-15 21:58:17 +0530

    Clock

---
+++ Logoical/Clock.java
@@ -0,0 +1,12 @@
+package java_prog.Logoical;
+
+import java.time.LocalDateTime;
+
+public class Clock {
+
+	public static void main(String[] args) {
+
+		System.out.println(LocalDateTime.now());
+	}
+
+}
