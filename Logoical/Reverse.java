39a9a39156a1028cd0d21ebad5b9001a578377be
keshav19199 <keshavwadhavane19@gmail.com>
2025-12-17 20:38:16 +0530

    Reverse String

---
+++ Logoical/Reverse.java
@@ -0,0 +1,14 @@
+package java_prog.Logoical;
+
+public class Reverse {
+
+	public static void main(String[] args) {
+
+		String str="KESHAV WADHAVANE";
+		
+		StringBuilder rev = new StringBuilder(str).reverse();
+		System.out.println(rev);
+		
+	}
+
+}
