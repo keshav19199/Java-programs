add92cceab2ca1356676c188eb98299fb6f9a2c1
keshav19199 <keshavwadhavane19@gmail.com>
2025-12-30 10:37:51 +0530

    logic

---
+++ Logoical/Check.java
@@ -0,0 +1,13 @@
+package java_prog.Logoical;
+
+public class Check {
+
+	public static void main(String[] args) {
+
+		int a=2;
+		char c='5';
+		
+		System.out.println(a*c);
+	}
+
+}
---
+++ Logoical/Check2.java
@@ -0,0 +1,19 @@
+package java_prog.Logoical;
+
+public class Check2 {
+
+	public static void main(String[] args) {
+
+		boolean x=false;
+		
+		if (x)
+		{
+			System.out.println("hii");
+		}
+		else
+		{
+			System.out.println("Heloo");
+		}
+	}
+
+}
