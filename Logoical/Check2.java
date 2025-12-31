e307c31b52ffdf8fc7dbe51e21b6283bee92f8ca
keshav19199 <keshavwadhavane19@gmail.com>
2025-12-31 11:19:09 +0530

    logic

--- Logoical/Check2.java
+++ Logoical/Check2.java
@@ -2,6 +2,7 @@ package java_prog.Logoical;
 
 public class Check2 {
 
+	
 	public static void main(String[] args) {
 
 		boolean x=false;
---
+++ Logoical/RevStrBuild.java
@@ -0,0 +1,14 @@
+package java_prog.Logoical;
+
+public class RevStrBuild {
+
+	public static void main(String[] args) {
+
+		String str="Keshav";
+		
+		StringBuilder rev=new StringBuilder(str).reverse();
+		
+		System.out.println(rev);
+	}
+
+}
