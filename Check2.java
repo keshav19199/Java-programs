220a781c94a048e3dae20e8b69648d7da13b61b0
keshav19199 <keshavwadhavane19@gmail.com>
2026-01-01 22:25:45 +0530

    demo

---
+++ Check2.java
@@ -0,0 +1,30 @@
+package java_prog;
+
+public class Check2 {
+
+	public static void main(String[] args) {
+
+		byte a=10;
+		byte b=20;
+		
+		Letter l=new Letter(a,b);
+		Letter l2=new Letter(a,b);
+		
+		System.out.println(l.equals(l2));
+		System.out.println(l.hashCode());
+		System.out.println(l2.hashCode());
+		
+	}
+}
+
+ class Letter{
+	Letter(int a,int b)
+	{
+		System.out.println("a="+a+"b="+b);
+	}
+
+	Letter(float a,float b)
+	{
+		System.out.println("a="+a+"b"+b);
+	}
+}
---
+++ check1.java
@@ -0,0 +1,16 @@
+package java_prog;
+
+public class check1 {
+
+	public static void main(String[] args) {
+		
+		System.out.println(10+20+"Hello");
+		System.out.println("Hello"+10+12);
+		
+		System.out.println("**************");
+		
+		System.out.println(10*2+"Hello");
+		System.out.println("Hello"+10*2);
+	}
+
+}
