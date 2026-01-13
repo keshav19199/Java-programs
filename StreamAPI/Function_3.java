1321f9062ca9cec82a7498f2b5cb31a22ea645ae
keshav19199 <keshavwadhavane19@gmail.com>
2026-01-13 22:29:34 +0530

    function

--- StreamAPI/Function_3.java
+++ StreamAPI/Function_3.java
@@ -14,11 +14,16 @@ public class Function_3 {
 		list.add("Hiii...");
 		list.add("keshava madhava");
 		
+		// reverce the string with string API
+		//  to lower case
+		
+		Function<String, String> fun2= (s)->{return s.toUpperCase();};
+		
 		Function<String, StringBuffer> fun1=(s)->{return new StringBuffer(s).reverse();};
 		Consumer<StringBuffer> co=(i)->{System.out.println(i);};
 		
 		System.out.println(list);
-		list.stream().map(fun1).forEach(co);;
+		list.stream().map(fun2).map(fun1).forEach(co);;
 		
 		
 	}
