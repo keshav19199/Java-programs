5ecb57f0757d0b8aae31224e74c31b0e07f9df99
keshav19199 <keshavwadhavane19@gmail.com>
2026-01-22 15:20:56 +0530

    logic

---
+++ StreamAPI/Consumer_4.java
@@ -0,0 +1,19 @@
+package java_prog.StreamAPI;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class Consumer_4 {
+
+	public static void main(String[] args) {
+		 
+		
+		List<String> li = Arrays.asList("AKLFJ","AMDKF","KAJ","kjaij");
+		List<String> li1 = Arrays.asList("AKLFJ","AMDKF","KAJ","kjaij");
+		List<String> li2 = Arrays.asList("AKLFJ","AMDKF","KAJ","kjaij");
+		
+		
+
+	}
+
+}
