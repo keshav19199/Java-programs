fbe7cf520424232e194436b49aacb5e652427f2d
keshav19199 <keshavwadhavane19@gmail.com>
2026-02-16 16:09:19 +0530

    logic

---
+++ Logical2/RevSrtingBuild2.java
@@ -0,0 +1,14 @@
+package java_prog.Logical2;
+
+public class RevSrtingBuild2 {
+
+	public static void main(String[] args) {
+
+		String str="Helloo jii..";
+		
+		StringBuilder sb= new StringBuilder(str);
+				
+		System.out.println(sb.reverse());
+	}
+
+}
