a79692bf5fec4cbf2a452d46100b23f5866ea19a
keshav19199 <keshavwadhavane19@gmail.com>
2026-02-16 16:12:31 +0530

    logic

---
+++ Logical2/StringBuff_1.java
@@ -0,0 +1,14 @@
+package java_prog.Logical2;
+
+public class StringBuff_1 {
+
+	public static void main(String[] args) {
+
+		String str="Hii..";
+		
+		StringBuffer sb = new StringBuffer(str);
+		
+		System.out.println(sb.reverse());
+	}
+
+}
