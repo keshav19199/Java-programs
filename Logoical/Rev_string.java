7a8daf6be11f8bcb7f4e1d9579c0b0b4e62662c9
keshav19199 <keshavwadhavane19@gmail.com>
2026-02-20 20:48:08 +0530

    logic

---
+++ Logoical/Rev_string.java
@@ -0,0 +1,14 @@
+package java_prog.Logoical;
+
+public class Rev_string {
+
+	public static void main(String[] args) {
+
+		String str="kkeeshha";
+		
+		StringBuilder sb=new StringBuilder(str);
+		
+		System.out.println(sb.reverse());
+	}
+
+}
