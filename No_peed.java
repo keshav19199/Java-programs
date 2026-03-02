5dfc275ffa20e909ee495e4241a985fe6e25d6ee
keshav19199 <keshavwadhavane19@gmail.com>
2026-03-02 14:55:52 +0530

    basic

---
+++ No_peed.java
@@ -0,0 +1,22 @@
+package java_prog;
+
+public class No_peed {
+
+	public static void main(String[] args) {
+
+//	long n=1000000000;
+//		for(int i=0; i<n; i++)
+//		{
+//			System.out.println(i);
+//	
+	int i=1;
+	while(true)
+	{
+		System.out.println(i);
+		i++;
+	}
+	
+	
+	}
+
+}
