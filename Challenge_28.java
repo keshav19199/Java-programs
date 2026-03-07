fcf59cbc1fd9e2a049918a542a8f8293b21ebbda
keshav19199 <keshavwadhavane19@gmail.com>
2026-03-08 00:08:57 +0530

    table

---
+++ Challenge_28.java
@@ -0,0 +1,22 @@
+// print the table.
+
+package java_prog;
+
+public class Challenge_28 {
+
+	public static void main(String[] args) {
+
+		int num=30;
+		for(int i=1; i<=num; i++)
+		{
+			for(int j=1; j<=10; j++)
+			{
+				System.out.print(" "+i*j);
+			}
+			System.out.println();
+			System.out.println("***********************************");
+		}
+		System.out.println();
+	}
+
+}
