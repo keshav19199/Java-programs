74ab03f6dd4cc8f17aa229a0b2e84afaf5f0a2c1
keshav19199 <keshavwadhavane19@gmail.com>
2026-03-07 23:24:16 +0530

    sum & average

--- Challenge_40.java
+++ Challenge_40.java
@@ -3,8 +3,18 @@ package java_prog;
 public class Challenge_40 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 
+		int count=0;
+		int	 sum=0;
+		
+		for(int i=0; i<=10; i++)
+		{
+			sum+=i;
+			count++;
+		}
+		System.out.println("Sum: "+sum);
+		System.out.println("average: "+(double)sum/count);
+		
 	}
 
 }
