85bded224c1f5772a1013cc5e6fdf01232e5c1e8
keshav19199 <keshavwadhavane19@gmail.com>
2026-03-23 16:41:01 +0530

    logic

---
+++ Revision/Parameter_1.java
@@ -0,0 +1,21 @@
+package java_prog.Revision;
+
+public class Parameter_1 {
+
+	public static void main(String[] args) {
+
+//		int num=TwoParameter(4,2);
+//		System.out.println(num);
+		
+		System.out.println(TwoParameter(4,3));
+		
+		// it's known as a parameter or arguments
+		
+	}
+
+	public static int TwoParameter(int first, int second)
+	{
+		int sum=first+second;
+		return sum;
+	}
+}
---
+++ challeges/CH_33.java
@@ -0,0 +1,10 @@
+package java_prog.challeges;
+
+public class CH_33 {
+
+	public static void main(String[] args) {
+
+		
+	}
+
+}
