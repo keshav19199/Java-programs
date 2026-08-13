03de78c311480f550ef46d1c5e4dd44314ede088
keshav19199 <keshavwadhavane19@gmail.com>
2026-08-13 22:53:18 +0530

    logical codes

--- StreamAPI/logics/Even_No.java
+++ StreamAPI/logics/Even_No.java
@@ -9,7 +9,7 @@ public class Even_No {
 		List.of(12,13,15,17,6,43,54,65,87)
 		.stream()
 		.filter((i)->(i%2==0))
-		.forEach((i)->System.out.println(i));
+		.forEach((i)->{System.out.println(i);});
 	}
 
 }
