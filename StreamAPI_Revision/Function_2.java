29fbdde626ac2b28862c01d37aa8f4ea99154dc7
keshav19199 <keshavwadhavane19@gmail.com>
2026-08-25 15:02:20 +0530

    supplier

--- StreamAPI_Revision/Function_2.java
+++ StreamAPI_Revision/Function_2.java
@@ -7,8 +7,14 @@ public class Function_2 {
 	public static void main(String[] args) {
 
 		Function<String, Integer> fun=(s)-> s.length();
+		Function<String, String> fun2=(s1)-> s1.concat("Okay");
+		
+		Function<String, Integer> fun3=fun.compose(fun2);
 		
 		System.out.println(fun.apply("Keshav wadhavane"));
+		System.out.println(fun2.apply("Hello"));
+		
+		System.out.println(fun3.apply("k"));
 	}
 
 }
