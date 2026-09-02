4f5517d257ff8ce2638c29287c9c52af44743a25
keshav19199 <keshavwadhavane19@gmail.com>
2026-09-02 13:53:30 +0530

    hello

---
+++ multiTreading/Revision/ListAdd.java
@@ -0,0 +1,58 @@
+package java_prog.multiTreading.Revision;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ListAdd {
+
+	public static void main(String[] args) throws InterruptedException {
+
+		System.out.println("Hello");
+		List<Integer> list=new ArrayList<Integer>();
+		First1 f=new First1(list);
+		Second s=new Second(list);
+		
+		f.start();
+		s.start();
+		
+		f.join();
+		s.join();
+		
+		System.out.println(list.size());
+				
+		
+	}
+}
+class First1 extends Thread
+{
+	List<Integer> list;
+	public void run()
+	{
+		for(int i=0; i<1000; i++)
+		{
+			list.add(i);
+		}
+	}
+	public First1(List<Integer> list) {
+		super();
+		this.list = list;
+	}
+	
+}
+class Second extends Thread
+{
+	List<Integer> list;
+	public void run()
+	{
+		for(int i=0; i<1000; i++)
+		{
+			list.add(i);
+		}
+	}
+	public Second(List<Integer> list) {
+		super();
+		this.list = list;
+	}
+	
+	
+}
